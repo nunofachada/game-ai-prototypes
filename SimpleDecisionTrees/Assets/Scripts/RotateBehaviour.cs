019c4f0372eefde4ae96b8c9ed1c58e927f95f93
Nuno Fachada <nuno.fachada@ulusofona.pt>
2019-11-21 18:02:32 +0000

    Add a simple decision trees example

---
+++ SimpleDecisionTrees/Assets/Scripts/RotateBehaviour.cs
@@ -0,0 +1,22 @@
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Author: Nuno Fachada
+ * */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 rotateSpeed = new Vector3(0f, 3f, 0f);
+
+    // Rotate the object
+    private void Update()
+    {
+        transform.Rotate(rotateSpeed);
+    }
+}
