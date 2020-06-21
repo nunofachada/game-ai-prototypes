b41ac759da9a6aab64235d6e35c131a8049a543b
Nuno Fachada <nuno.fachada@ulusofona.pt>
2020-06-21 23:37:02 +0100

    VisualRNGs: DispersionGeneration cleanup

--- VisualRNGs/Assets/Scripts/DispersionGeneration.cs
+++ VisualRNGs/Assets/Scripts/DispersionGeneration.cs
@@ -1,42 +1,80 @@
-﻿using UnityEngine;
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Author: Nuno Fachada
+ * */
+
+using UnityEngine;
 using UnityEngine.UI;
 using LibGameAI.RNG;
+using Random = System.Random;
 
 public class DispersionGeneration : MonoBehaviour
 {
+    public enum RNG { System, Halton }
 
-    private RawImage image;
-    private Color[] pixels;
+    [SerializeField]
+    [Range(0.1f, 100f)]
+    private float scale = 1;
+
+    [SerializeField]
+    private int numberOfPoints = 1000;
+
+    [SerializeField]
+    private RNG xRandomNumberGenerator = RNG.System;
+
+    [SerializeField]
+    private int xSeed = 2;
+
+    [SerializeField]
+    private RNG yRandomNumberGenerator = RNG.System;
+
+    [SerializeField]
+    private int ySeed = 3;
 
     private void Awake()
     {
-        //System.Random rnd = new System.Random();
-        //System.Random rnd = new LCG();
-        System.Random rnd = new XorShift128();
-        System.Random rnd1 = new Halton(3);
-        System.Random rnd2 = new Halton(179);
+        // Image width and height
+        int width = (int)(Screen.width / scale);
+        int height = (int)(Screen.height / scale);
 
-        image = GetComponent<RawImage>();
-        pixels = new Color[Screen.width * Screen.height];
-        for (int i = 0; i < Screen.height * Screen.width; i++)
+        // Texture to show on screen, to be randomly created
+        Texture2D texture = new Texture2D(width, height);
+
+        // Get image component where to place the texture
+        RawImage image = GetComponent<RawImage>();
+
+        // Instantiate random number generator for x coordinate
+        Random xRand = xRandomNumberGenerator == RNG.System
+            ? new Random(xSeed)
+            : new Halton(xSeed);
+
+        // Instantiate random number generator for y coordinate
+        Random yRand = yRandomNumberGenerator == RNG.System
+            ? new Random(ySeed)
+            : new Halton(ySeed);
+
+        // Create a vector of pixels and set them all to white
+        Color[] pixels = new Color[width * height];
+
+        for (int i = 0; i < height * width; i++)
             pixels[i] = Color.white;
 
-        Texture2D text = new Texture2D(Screen.width, Screen.height);
-
-        for (int i = 0; i < Screen.height * Screen.width / 5; i++)
+        // Place the specified number of points
+        for (int i = 0; i < numberOfPoints; i++)
         {
-            // int x = rnd.Next(Screen.width);
-            // int y = rnd.Next(Screen.height);
-            int x = rnd1.Next(Screen.width);
-            int y = rnd2.Next(Screen.height);
-            //if (i % 1000 == 0) Debug.Log($"{i}: {Halton.Sequence(2, i)}");
-            pixels[y * Screen.width + x] = Color.black;
+            int x = xRand.Next(width);
+            int y = yRand.Next(height);
+            pixels[y * width + x] = Color.black;
         }
 
-        text.SetPixels(pixels);
-        text.Apply();
-        image.texture = text;
+        // Set and apply texture pixels
+        texture.SetPixels(pixels);
+        texture.Apply();
 
+        // Place texture in image
+        image.texture = texture;
     }
 
 }
