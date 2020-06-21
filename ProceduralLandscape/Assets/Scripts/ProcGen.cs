8d7e2d1094e25aa0bf1b0ec28753ae0da64278ec
Nuno Fachada <nuno.fachada@ulusofona.pt>
2020-06-21 13:08:38 +0100

    Procedural Landscape: first version with Perlin noise

---
+++ ProceduralLandscape/Assets/Scripts/ProcGen.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ProcGen : MonoBehaviour
+{
+    [SerializeField]
+    private float tileSize = 10f;
+
+    [SerializeField]
+    [Range(0,1)]
+    private float maxAltitude = 0.1f;
+
+    private void Awake()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        int width = terrain.terrainData.heightmapWidth;
+        int height = terrain.terrainData.heightmapHeight;
+
+        float[,] heights = new float[width, height];
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                heights[i, j] = maxAltitude* Mathf.PerlinNoise(
+                    tileSize * i / (float)width, tileSize * (float)j / height);
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, heights);
+        //terrain.terrainData.SyncHeightmap();
+    }
+
+}
---
+++ ProceduralLandscape/README.md
@@ -0,0 +1,7 @@
+# Procedural Landscape
+
+## Assets in use
+
+* [Mysteryem](https://opengameart.org/node/8070) by
+  [qubodup](https://opengameart.org/users/qubodup)
+  ([CC-BY-SA 3.0](http://creativecommons.org/licenses/by-sa/3.0/))
