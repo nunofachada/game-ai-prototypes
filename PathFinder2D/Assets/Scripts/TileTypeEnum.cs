dd6cc227e70eecd3d413e072c91d8571d0373fb3
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-04-14 23:19:38 +0100

    PathFinder2D: add license header

--- PathFinder2D/Assets/Scripts/TileTypeEnum.cs
+++ PathFinder2D/Assets/Scripts/TileTypeEnum.cs
@@ -1,4 +1,11 @@
-// Enumeration of stuff that can be in the tile-based world
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Author: Nuno Fachada
+ * */
+
+// State of a tile in the tile-based world
 public enum TileTypeEnum
 {
     // Empty tile
