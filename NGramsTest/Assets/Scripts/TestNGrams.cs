ef9286526e61e2d451c2c4af6a7bad6e3a5daba1
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-05-05 23:17:55 +0100

    NGramsTest: update to Unity 2020.1.17f + use NaughtyAttributes to show/hide threshold for hierarchical N-Grams

--- NGramsTest/Assets/Scripts/TestNGrams.cs
+++ NGramsTest/Assets/Scripts/TestNGrams.cs
@@ -4,127 +4,137 @@
  *
  * Author: Nuno Fachada
  * */
+
 using System.Collections.Generic;
 using UnityEngine;
 using NGrams;
+using NaughtyAttributes;
 
-public class TestNGrams : MonoBehaviour
+namespace AIUnityExamples.NGramsTest
 {
-    [Tooltip("The N value in the N-Gram")]
-    [Range(0, 12)]
-    [SerializeField] private int nValue = 4;
-
-    [Tooltip("Show accuracy after how many clicks?")]
-    [Range(2, 20)]
-    [SerializeField] private int showAccuracyInterval = 10;
-
-
-    [Tooltip("Use hierarchical N-Gram?")]
-    [SerializeField] private bool useHierarchicalNGram = true;
-
-    [Tooltip("How many times a sequence has to be seen in order to " +
-        "make a prediction (hierarchical N-Grams only)")]
-    [Range(1, 12)]
-    [SerializeField] private int threshold = 3;
-
-    // Reference to the N-Gram predictor
-    private INGram<char> predictor;
-
-    // List of mouse presses (L or R)
-    private List<char> listOfChars;
-
-    // Right and wrong predictions
-    private int right, wrong;
-
-    // Last prediction
-    private char prediction;
-
-    // Use this for initialization
-    private void Start()
+    public class TestNGrams : MonoBehaviour
     {
-        // Initialize N-Gram
-        predictor = useHierarchicalNGram
-            ? new HierarchNGram<char>(nValue, threshold) as INGram<char>
-            : new NGram<char>(nValue) as INGram<char>;
+        [Tooltip("The N value in the N-Gram")]
+        [Range(0, 12)]
+        [SerializeField]
+        private int nValue = 4;
 
-        // Initialize list of chars
-        listOfChars = new List<char>();
+        [Tooltip("Show accuracy after how many clicks?")]
+        [Range(2, 20)]
+        [SerializeField]
+        private int showAccuracyInterval = 10;
 
-        // Initialize right and wrong predictions
-        right = 0;
-        wrong = 0;
 
-        // Initialize prediction to none
-        prediction = default(char);
-    }
+        [Tooltip("Use hierarchical N-Gram?")]
+        [SerializeField]
+        private bool useHierarchicalNGram = true;
 
-    // Update is called once per frame
-    private void Update()
-    {
-        // Whether the user clicked the mouse buttons in this frame
-        bool clicked = false;
+        [Tooltip("How many times a sequence has to be seen in order to " +
+            "make a prediction (hierarchical N-Grams only)")]
+        [Range(1, 12)]
+        [SerializeField]
+        [ShowIf(nameof(useHierarchicalNGram))]
+        private int threshold = 3;
 
-        // Capture mouse button presses
-        if (Input.GetMouseButtonDown(0))
+        // Reference to the N-Gram predictor
+        private INGram<char> predictor;
+
+        // List of mouse presses (L or R)
+        private List<char> listOfChars;
+
+        // Right and wrong predictions
+        private int right, wrong;
+
+        // Last prediction
+        private char prediction;
+
+        // Use this for initialization
+        private void Start()
         {
-            listOfChars.Add('L');
-            clicked = true;
+            // Initialize N-Gram
+            predictor = useHierarchicalNGram
+                ? new HierarchNGram<char>(nValue, threshold) as INGram<char>
+                : new NGram<char>(nValue) as INGram<char>;
+
+            // Initialize list of chars
+            listOfChars = new List<char>();
+
+            // Initialize right and wrong predictions
+            right = 0;
+            wrong = 0;
+
+            // Initialize prediction to none
+            prediction = default(char);
         }
 
-        if (Input.GetMouseButtonDown(1))
+        // Update is called once per frame
+        private void Update()
         {
-            listOfChars.Add('R');
-            clicked = true;
+            // Whether the user clicked the mouse buttons in this frame
+            bool clicked = false;
+
+            // Capture mouse button presses
+            if (Input.GetMouseButtonDown(0))
+            {
+                listOfChars.Add('L');
+                clicked = true;
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                listOfChars.Add('R');
+                clicked = true;
+            }
+
+            // Did the user clicked the mouse buttons in this frame?
+            if (clicked)
+            {
+                // Show input and what we had predicted with the N-Gram
+                Debug.Log($"Input='{listOfChars[listOfChars.Count - 1]}' "
+                    + $"| Prediction='{prediction}'");
+
+                // Determine if prediction was accurate and update right or wrong
+                // accordingly
+                if (listOfChars[listOfChars.Count - 1] == prediction) right++;
+                else wrong++;
+
+                // Time to show accuracy?
+                if ((right + wrong) % showAccuracyInterval == 0)
+                    Debug.LogWarning(
+                        $"Current accuracy: {((float)right) / (right + wrong):p}");
+
+                // Register sequence
+                predictor.RegisterSequence(listOfChars.ToArray());
+
+                // Remove first if list equal or larger than nValue
+                if (listOfChars.Count >= nValue)
+                    listOfChars.RemoveAt(0);
+
+                // Make prediction for next input
+                prediction = predictor.GetMostLikely(listOfChars.ToArray());
+            }
         }
 
-        // Did the user clicked the mouse buttons in this frame?
-        if (clicked)
+        // Show the final accuracy when the application terminates
+        private void OnApplicationQuit()
         {
-            // Show input and what we had predicted with the N-Gram
-            Debug.Log($"Input='{listOfChars[listOfChars.Count - 1]}' "
-                + $"| Prediction='{prediction}'");
-
-            // Determine if prediction was accurate and update right or wrong
-            // accordingly
-            if (listOfChars[listOfChars.Count - 1] == prediction) right++;
-            else wrong++;
-
-            // Time to show accuracy?
-            if ((right + wrong) % showAccuracyInterval == 0)
+            if (right + wrong > 0)
                 Debug.LogWarning(
-                    $"Current accuracy: {((float)right) / (right + wrong):p}");
-
-            // Register sequence
-            predictor.RegisterSequence(listOfChars.ToArray());
-
-            // Remove first if list equal or larger than nValue
-            if (listOfChars.Count >= nValue)
-                listOfChars.RemoveAt(0);
-
-            // Make prediction for next input
-            prediction = predictor.GetMostLikely(listOfChars.ToArray());
+                    $"FINAL ACCURACY: {((float)right) / (right + wrong):p}");
         }
-    }
 
-    // Show the final accuracy when the application terminates
-    private void OnApplicationQuit()
-    {
-        if (right + wrong > 0)
-            Debug.LogWarning(
-                $"FINAL ACCURACY: {((float)right) / (right + wrong):p}");
-    }
+        // Show instructions
+        private void OnGUI()
+        {
+            GUI.color = Color.yellow;
 
-    // Show instructions
-    private void OnGUI()
-    {
-        GUI.color = Color.yellow;
-
-        GUI.Label(
-            new Rect(
-                Screen.width / 4,
-                Screen.height / 3,
-                Screen.width / 2,
-                Screen.height / 3),
-            "Click right and left mouse buttons and check console output");
+            GUI.Label(
+                new Rect(
+                    Screen.width / 4,
+                    Screen.height / 3,
+                    Screen.width / 2,
+                    Screen.height / 3),
+                "Click right and left mouse buttons and check console output");
+        }
     }
 }
