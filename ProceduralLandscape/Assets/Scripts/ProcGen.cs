0b3f34034d1539aff84e7995028e2621a91a21ae
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-05-16 01:00:13 +0100

    ProceduralLandscape: editor generation + camera rotation play mode

--- ProceduralLandscape/Assets/Scripts/ProcGen.cs
+++ ProceduralLandscape/Assets/Scripts/ProcGen.cs
@@ -5,25 +5,30 @@
  * Author: Nuno Fachada
  * */
 
+using System.Collections.Generic;
 using UnityEngine;
 using LibGameAI.ProcGen;
+using NaughtyAttributes;
 
 public class ProcGen : MonoBehaviour
 {
+
     [Header("General parameters")]
-
-    [SerializeField]
-    private float tileSize = 10f;
-
-    [SerializeField]
-    [Range(0, 1)]
-    private float maxAltitude = 0.1f;
 
     [SerializeField]
     private int seed = 1234;
 
     [SerializeField]
+    [Range(0, 1)]
+    private float maxAltitude = 0.1f;
+
+    [Header("Perlin noise")]
+
+    [SerializeField]
     private bool perlinNoise = true;
+
+    [SerializeField]
+    private float tileSize = 10f;
 
     [Header("Fault parameters")]
 
@@ -65,7 +70,8 @@ public class ProcGen : MonoBehaviour
     // [Range(0, 1)]
     // private float maxHeight = 0;
 
-    private void Awake()
+    [Button("Generate", enabledMode: EButtonEnableMode.Editor)]
+    private void Generate()
     {
         System.Random random = new System.Random(seed);
 
@@ -137,6 +143,60 @@ public class ProcGen : MonoBehaviour
 
         // Apply terrain heights
         terrain.terrainData.SetHeights(0, 0, heights);
+    }
+
+    [Button("Clear", enabledMode: EButtonEnableMode.Editor)]
+    private void Clear()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        int width = terrain.terrainData.heightmapResolution;
+        int height = terrain.terrainData.heightmapResolution;
+
+        float[,] heights = new float[width, height];
+
+        // Apply terrain heights
+        terrain.terrainData.SetHeights(0, 0, heights);
+
+    }
+
+
+    // Update is called once per frame
+    // We use to orbit the camera around the cube grid
+    private void Update()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        float width = terrain.terrainData.size.x;
+        float height = terrain.terrainData.size.z;
+
+        // Determine the radius which the camera will orbit around the
+        // center of the grid
+        float radius = (width + height) / 3f;
+
+        // Camera rotation speed
+        float cameraRotationSpeed = 0.25f;
+
+
+        // Determine camera position in its orbit around the center of the grid
+        Camera.main.transform.position = new Vector3(width / 2, 1700 * maxAltitude, height / 2)
+            + radius * new Vector3(
+                Mathf.Sin(cameraRotationSpeed * Time.time),
+                0,
+                Mathf.Cos(cameraRotationSpeed * Time.time));
+
+        // Make camera look at center of the grid
+        Camera.main.transform.LookAt(new Vector3(width / 2, 0, height / 2));
+
+    }
+
+    // private void OnDrawGizmos()
+    // {
+    //     Gizmos.DrawLine(Camera.main.transform.position, new Vector3(500, 0, 500));
+    //     Gizmos.DrawSphere(new Vector3(500, 1000 * maxAltitude, 500), 20);
+    // }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), $"Dist: {(Camera.main.transform.position - new Vector3(500, 1500 * maxAltitude, 500)).magnitude}");
     }
 
 }
