c8e74cd67058cf273d08758f829740966540d836
Nuno Fachada <nuno.fachada@ulusofona.pt>
2022-05-19 23:16:19 +0100

    NGramsFight: add new ListPlusOneWrapper collection

---
+++ NGramsFight/Assets/Scripts/Util/ListPlusOneWrapper.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LibGameAI.Util
+{
+    public struct ListPlusOneWrapper<T> : IReadOnlyList<T>
+    {
+        public int Count { get; }
+        private IReadOnlyList<T> list;
+        private T plusOne;
+
+        public ListPlusOneWrapper(IReadOnlyList<T> list, T plusOne)
+        {
+            this.list = list;
+            this.plusOne = plusOne;
+            Count = list.Count + 1;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < list.Count)
+                {
+                    return list[index];
+                }
+                else
+                {
+                    return plusOne;
+                }
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in list) yield return item;
+            yield return plusOne;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
