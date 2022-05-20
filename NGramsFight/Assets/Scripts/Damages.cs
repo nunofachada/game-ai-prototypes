21a4d3f5507904cf755766688523143ae20fba23
Nuno Fachada <nuno.fachada@ulusofona.pt>
2022-05-20 17:29:19 +0100

    NGramsFight: move patters and damage config to monobehaviours instead of scriptable objects
    Was more flexible with scriptable objects, but it's more user
    friendly with monobehaviours

--- NGramsFight/Assets/Scripts/Damages.cs
+++ NGramsFight/Assets/Scripts/Damages.cs
@@ -7,11 +7,60 @@ namespace AIUnityExample.NGramsFight
 {
     public class Damages : MonoBehaviour
     {
-
         [SerializeField]
-        [Expandable]
-        private DamagesConfig damagesConfig;
+        [ReorderableList]
+        private List<AttackDefenseDamage> damages;
 
+        private void Awake()
+        {
+            var set = new HashSet<AttackDefenseDamage>(damages);
+            int expectedSetSize = 0;
+            for (int i = 0; i < damages.Count; i++)
+            {
+                set.Add(damages[i]);
+                expectedSetSize++;
+                if (set.Count < expectedSetSize)
+                {
+                    damages.RemoveAt(i);
+                    expectedSetSize--;
+                    Debug.LogWarning(
+                        $"Removing repeated attack '{damages[i]}' from damage configuration.");
+                }
+            }
+        }
+
+        [Button]
+        private void Clear()
+        {
+            if (damages is null)
+            {
+                damages = new List<AttackDefenseDamage>();
+            }
+            else
+            {
+                damages.Clear();
+            }
+        }
+
+        [Button]
+        private void SetDefaults()
+        {
+            Clear();
+
+            // High attack
+            damages.Add(new AttackDefenseDamage(AttackType.High, DefenseType.High, 2f, 1.8f));
+            // Med attack
+            damages.Add(new AttackDefenseDamage(AttackType.Med, DefenseType.Med, 1.5f, 1.1f));
+            // Low attack
+            damages.Add(new AttackDefenseDamage(AttackType.Low, DefenseType.Low, 1.2f, 0.6f));
+            // MegaTop attack
+            damages.Add(new AttackDefenseDamage(AttackType.MegaTop, DefenseType.High, 5f, 6f));
+            // SuperMean attack
+            damages.Add(new AttackDefenseDamage(AttackType.SuperMean, DefenseType.Med, 5.5f, 7f));
+            // HyperUnder attack
+            damages.Add(new AttackDefenseDamage(AttackType.HyperUnder, DefenseType.Low, 6f, 8f));
+
+        }
 
     }
 }
--- NGramsFight/Assets/Scripts/Patterns.cs
+++ NGramsFight/Assets/Scripts/Patterns.cs
@@ -7,8 +7,41 @@ namespace AIUnityExample.NGramsFight
     public class Patterns : MonoBehaviour
     {
         [SerializeField]
-        [Expandable]
-        private PatternsConfig patternsConfig;
+        [ReorderableList]
+        private List<AttackPattern> patterns;
+
+        [Button]
+        private void Clear()
+        {
+            if (patterns is null)
+            {
+                patterns = new List<AttackPattern>();
+            }
+            else
+            {
+                patterns.Clear();
+            }
+        }
+
+        [Button]
+        private void SetDefaults()
+        {
+            Clear();
+
+            // Low attack
+            patterns.Add(new AttackPattern("d,s,s,s", AttackType.Low));
+            // Med attack
+            patterns.Add(new AttackPattern("d,a,d,d", AttackType.Med));
+            // High attack
+            patterns.Add(new AttackPattern("d,w,w,w", AttackType.High));
+            // Mega attack
+            patterns.Add(new AttackPattern("d,w,w,d,d", AttackType.MegaTop));
+            // Super attack
+            patterns.Add(new AttackPattern("d,a,d,w,w", AttackType.SuperMean));
+            // Hyper attack
+            patterns.Add(new AttackPattern("d,s,s,w,s", AttackType.HyperUnder));
+
+        }
 
         private InputFrontend inputFrontend;
 
@@ -31,7 +64,7 @@ namespace AIUnityExample.NGramsFight
             MinLength = int.MaxValue;
             MaxLength = 0;
 
-            foreach (AttackPattern pattern in patternsConfig)
+            foreach (AttackPattern pattern in patterns)
             {
                 //Debug.Log(pattern + ", size=" + pattern.Size);
                 if (pattern.Size < MinLength) MinLength = pattern.Size;
