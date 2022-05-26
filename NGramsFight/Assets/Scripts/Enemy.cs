879fd9b162bd4045afcdadcf430ca30209fd7a74
Nuno Fachada <nuno.fachada@ulusofona.pt>
2022-05-26 21:23:42 +0100

    NGramsFight: doc-comment the Enemy class

--- NGramsFight/Assets/Scripts/Enemy.cs
+++ NGramsFight/Assets/Scripts/Enemy.cs
@@ -1,22 +1,37 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Author: Nuno Fachada
+
 using System.Collections;
 using UnityEngine;
 
 namespace AIUnityExample.NGramsFight
 {
+    /// <summary>
+    /// A script for handling the enemy agent.
+    /// </summary>
     public class Enemy : Agent
     {
+        [Tooltip("How long should the enemy keep the shield raised after predicting an attack?")]
         [SerializeField]
         private float defenseDuration = 1f;
 
+        // Reference to the shield game object
         [SerializeField]
         private GameObject shield;
 
+        // Used in the co-routine which keeps the shield raised
         private YieldInstruction defenseUpWait;
 
+        // The base position of the shield
         private Vector3 shieldBasePos;
 
-        public DefenseType? Defense { get; private set; }
+        // The current raised defense, if any
+        private DefenseType? defense;
 
+        // Called when the script instance is being loaded
         protected override void Awake()
         {
             base.Awake();
@@ -25,22 +40,38 @@ namespace AIUnityExample.NGramsFight
             shield.SetActive(false);
         }
 
-        public void SendPrediction(AttackType attack)
+        /// <summary>
+        /// Informs the enemy of the prediction for the next attack.
+        /// </summary>
+        /// <param name="attack">The predicted attack.</param>
+        public void ReceivePrediction(AttackType attack)
         {
-            if (!Defense.HasValue)
+            // Mount a defense, but only if no defense is currently mounted
+            if (!defense.HasValue)
             {
+                // Determine proper defense
                 AttackDefenseDamage attDefDam = GetAttackDefenseDamage(attack);
+
+                // Initiate proper defense
                 StartCoroutine(DefenseUp(attDefDam.ProperDefense));
             }
         }
 
+        // Co-routine which keeps a defense raised during a predefined duration
         private IEnumerator DefenseUp(DefenseType defense)
         {
+            // Activate shield and put it into place
             shield.SetActive(true);
             shield.transform.localPosition = shieldBasePos + new Vector3(0, 0.181f * (int)defense , 0);
-            Defense = defense;
+
+            // Keep a note of the defense currently raised
+            this.defense = defense;
+
+            // Wait for a predefined duration before unmounting defense
             yield return defenseUpWait;
-            Defense = null;
+
+            // Deactivate shield / unmount defense
+            this.defense = null;
             shield.SetActive(false);
         }
     }
