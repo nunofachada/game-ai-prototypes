cb8e4146395787ee486791cb1d99c7ce218a1ae6
Nuno Fachada <nuno.fachada@ulusofona.pt>
2022-05-26 21:22:57 +0100

    NGramsFight: doc-comment the DefenseType and AttackType enums

--- NGramsFight/Assets/Scripts/AttackType.cs
+++ NGramsFight/Assets/Scripts/AttackType.cs
@@ -1,12 +1,44 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Author: Nuno Fachada
+
 namespace AIUnityExample.NGramsFight
 {
+    /// <summary>
+    /// The various attack types.
+    /// </summary>
     public enum AttackType
     {
+        /// <summary>
+        /// Regular low attack.
+        /// </summary>
         Low,
+
+        /// <summary>
+        /// Regular med attack.
+        /// </summary>
         Med,
+
+        /// <summary>
+        /// Regular high attack.
+        /// </summary>
         High,
+
+        /// <summary>
+        /// MegaMean special attack.
+        /// </summary>
         MegaMean,
+
+        /// <summary>
+        /// SuperUnder special attack.
+        /// </summary>
         SuperUnder,
+
+        /// <summary>
+        /// HyperTop special attack.
+        /// </summary>
         HyperTop,
     }
 }
--- NGramsFight/Assets/Scripts/DefenseType.cs
+++ NGramsFight/Assets/Scripts/DefenseType.cs
@@ -1,9 +1,29 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Author: Nuno Fachada
+
 namespace AIUnityExample.NGramsFight
 {
+    /// <summary>
+    /// The different types of defense.
+    /// </summary>
     public enum DefenseType
     {
+        /// <summary>
+        /// Low defense.
+        /// </summary>
         Low,
+
+        /// <summary>
+        /// Med defense.
+        /// </summary>
         Med,
+
+        /// <summary>
+        /// High defense.
+        /// </summary>
         High
     }
 }
