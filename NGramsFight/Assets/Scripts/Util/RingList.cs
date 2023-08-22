b8721a1f60c37d7d6562cce68fd0030624db0c53
Nuno Fachada <faken@fakenmc.com>
2023-08-23 00:34:16 +0100

    NGramsFight: update license in library code

--- NGramsFight/Assets/Scripts/Util/RingList.cs
+++ NGramsFight/Assets/Scripts/Util/RingList.cs
@@ -1,6 +1,6 @@
-// Copyright (c) 2022 Nuno Fachada
-// Distributed under the MIT License (See accompanying file LICENSE or copy
-// at http://opensource.org/licenses/MIT)
+/* Copyright (c) 2018-2023 Nuno Fachada and contributors
+ * Distributed under the MIT License (See accompanying file LICENSE or copy
+ * at http://opensource.org/licenses/MIT) */
 
 using System;
 using System.Collections;
