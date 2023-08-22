a180cad4d5c66a251324c17f8b36be121bd7a6b2
Nuno Fachada <faken@fakenmc.com>
2023-08-22 21:15:10 +0100

    Procedural3D: sync libgameai code

--- Procedural3D/Assets/Scripts/LibGameAI/Util/MMath.cs
+++ Procedural3D/Assets/Scripts/LibGameAI/Util/MMath.cs
@@ -1,16 +1,26 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * Author: Nuno Fachada
- * */
+/* Copyright (c) 2018-2023 Nuno Fachada and contributors
+ * Distributed under the MIT License (See accompanying file LICENSE or copy
+ * at http://opensource.org/licenses/MIT) */
 
 namespace LibGameAI.Util
 {
+    /// <summary>
+    /// Useful math functions.
+    /// </summary>
     public static class MMath
     {
-        // https://en.wikipedia.org/wiki/Primality_test
-        private static bool IsPrime(int n)
+        /// <summary>
+        /// Check whether an integer is prime or not.
+        /// </summary>
+        /// <param name="n">Integer to check for primality.</param>
+        /// <returns>True if integer is prime, false otherwise.</returns>
+        /// <remarks>
+        /// Basic implementation as described in
+        /// <seealso href="https://en.wikipedia.org/wiki/Primality_test">
+        /// Wikipedia.
+        /// </seealso>
+        /// </remarks>
+        public static bool IsPrime(int n)
         {
             if (n <= 3)
                 return n > 1;
@@ -27,6 +37,36 @@ namespace LibGameAI.Util
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Normalize a vector between the specified limits.
+        /// </summary>
+        /// <param name="vector">Vector to normalize, in-place.</param>
+        /// <param name="min">Minimum normalization range.</param>
+        /// <param name="max">Maximum normalization range.</param>
+        public static void Normalize(float[] vector, float min, float max)
+        {
+            float localMin = vector[0];
+            float localMax = vector[0];
+            float origRange, newRange;
+
+            // Determine minimum and maximum heights
+            foreach (float h in vector)
+            {
+                if (h < localMin) localMin = h;
+                else if (h > localMax) localMax = h;
+            }
+
+            // Determine original and new ranges
+            origRange = localMax - localMin;
+            newRange = max - min;
+
+            // Perform actual normalization
+            for (int i = 0; i < vector.Length; i++)
+            {
+                vector[i] = (vector[i] - localMin) * newRange / origRange + min;
+            }
         }
     }
 }
